9232566426bb26d710821b6185229065d109100b
yandd <982724342@qq.com>
2017-05-02 18:30:08 +0800

    add remember option when login

--- app/src/static/login/login.js
+++ app/src/static/login/login.js
@@ -1,12 +1,44 @@
 import electron from 'electron';
+import fs from 'fs';
+import path from 'path';
 
 const { ipcRenderer } = electron;
 
 const form = document.getElementById('login-form');
 
+const AUTH_INFO_PATH = path.join(__dirname, '../../../', 'auth_info.json');
+
+var authInfo = null;
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const username = document.getElementById('username-input').value;
   const password = document.getElementById('password-input').value;
+  const remember = document.getElementById('remember-input').checked;
+
+  if (remember) {
+    if (!(authInfo && authInfo.username === username && authInfo.password === password)) {
+      fs.writeFileSync(AUTH_INFO_PATH, JSON.stringify({
+        username: username,
+        password: password
+      }));
+    }
+  } else {
+    fs.existsSync(AUTH_INFO_PATH) && fs.unlinkSync(AUTH_INFO_PATH);
+  }
+
   ipcRenderer.send('login-message', [username, password]);
 });
+
+(function(){
+    if (fs.existsSync(AUTH_INFO_PATH)) {
+        authInfo = JSON.parse(fs.readFileSync(AUTH_INFO_PATH, 'utf8'));
+        if (authInfo && authInfo.username && (authInfo.password || authInfo.password === '')) {
+            document.getElementById('username-input').value = authInfo.username;
+            document.getElementById('password-input').value = authInfo.password;
+            document.getElementById('remember-input').checked = true;
+        } else {
+            fs.unlinkSync(AUTH_INFO_PATH)
+        }
+    }
+})();
